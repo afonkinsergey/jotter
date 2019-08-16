4a91e2c0fb2713ecb413d3c388c3378737497f1b
Sergey Afonkin <afonkinsergey@gmail.com>
2019-08-17 02:37:44 +0300

    Переделана функция принятия дружбы (статус друг и вторая запись реверс id). Функция удаления из друзей. Просмотр кто мой друг.

--- lib/jotter/user/friendship.ex
+++ lib/jotter/user/friendship.ex
@@ -7,10 +7,7 @@ defmodule Jotter.User.Friendship do
     # field :friend_id, :integer
     field :status, :string
 
-    # belongs_to :user, User
     belongs_to :user, User, foreign_key: :user_id
     belongs_to :friend, User, foreign_key: :friend_id
-    # belongs_to :father, User, [foreign_key: :father_id]
-    # belongs_to :mother, User, [foreign_key: :mother_id]
   end
 end
--- lib/jotter/user/user.ex
+++ lib/jotter/user/user.ex
@@ -21,6 +21,7 @@ defmodule Jotter.User do
     # has_many :users_friendship, User.Friendship #, join_keys: [user_id: :id, friend_id: :id]
     has_many :request_users, User.Friendship, foreign_key: :user_id, where: [status: "request"]
     has_many :who_request_me, User.Friendship, foreign_key: :friend_id, where: [status: "request"]
+    has_many :my_friends, User.Friendship, foreign_key: :user_id, where: [status: "friend"]
   end
 
   # ниже специальная функция, которая делает проверку перед тем как передать запись в бд
@@ -75,6 +76,7 @@ defmodule Jotter.User do
     |> Repo.one() # обращение к репе
   end
 
+  # Делаем запрос на добавление в друзья
   def send_friend_request(user_id, friend_id) when user_id != friend_id do # правильно ли так делать?
     with %User{id: ^user_id} = user <- Repo.get(User, user_id),
     %User{id: ^friend_id} <- Repo.get(User, friend_id) do
@@ -82,30 +84,100 @@ defmodule Jotter.User do
       |> Ecto.build_assoc(:request_users, friend_id: friend_id)
       |> Repo.insert()
     else
-      :nil -> {:error, "User id not found"}
+      nil -> {:error, "User id #{user_id} or friend id #{friend_id} not found"}
     end
   end
 
-  def who_request_me(user_id) do
-    User
-    |> Repo.get(user_id)
-    |> Repo.preload(:who_request_me)
+  # Узнаём кто мне отправил запрос в друзья
+  def who_friend_request_me(user_id) do
+    with %User{id: ^user_id} <- Repo.get(User, user_id) do
+      User
+      |> Repo.get(user_id)
+      |> Repo.preload(:who_request_me)
+    else
+      nil -> {:error, "User id #{user_id} not found"}
+    end
   end
 
-  def accept_user_request(user_id, friend_id) do
-    User.Friendship
-    |> Ecto.Query.where(user_id: ^user_id, friend_id: ^friend_id) #|> IO.inspect()
-    |> Repo.one()
-    |> Ecto.Changeset.change() |> IO.inspect()
-    |> Ecto.Changeset.put_change(:status, "friend")
-    |> Repo.update()
+  # Принимает запрос в друзья и добавляем строку об ответной дружбе за одну транзакцию без проверок
+  # def accept_friend_request1(user_id, friend_id) when user_id != friend_id do
+  #   with %User{id: ^user_id} <- Repo.get(User, user_id),
+  #   %User{id: ^friend_id} = user <- Repo.get(User, friend_id) do
+  #     Repo.transaction(fn ->
+  #       Ecto.Query.from(u in User.Friendship, where: [user_id: ^user_id, friend_id: ^friend_id, status: "request"])
+  #       |> Repo.update_all(set: [status: "friend"])
+  #       user
+  #       |> Ecto.build_assoc(:request_users, friend_id: user_id, status: "friend")
+  #       |> Repo.insert()
+  #     end)
+  #   else
+  #     nil -> {:error, "User id #{user_id} or friend id #{friend_id} not found"}
+  #   end
+  # end
+
+  # Принимаем запрос в друзья и добавляем строку об ответной дружбе за одну транзакцию с проверкой
+  def accept_friend_request(user_id, friend_id) when user_id != friend_id do
+    with %User{id: ^user_id} <- Repo.get(User, user_id),
+          %User{id: ^friend_id} <- Repo.get(User, friend_id),
+          {:ok, _} <- add_friend(user_id, friend_id) do
+      :ok
+    else
+      nil -> {:error, "User id #{user_id} or friend id #{friend_id} not found"}
+      {:error, _} -> {:error, "Can not add friendship"}
+    end
   end
 
-  def reject_user_request(user_id, friend_id) do
-    User.Friendship
-    |> Ecto.Query.where(user_id: ^user_id, friend_id: ^friend_id) #|> IO.inspect()
-    |> Repo.one()
-    |> Ecto.Changeset.change()
-    |> Repo.delete()
+  defp add_friend(user_id, friend_id) do
+    Repo.transaction(fn ->
+      # Ecto.Query.from(u in User.Friendship, where: [user_id: ^user_id, friend_id: ^friend_id, status: "request"])
+      # |> Repo.one!()
+      User.Friendship
+      |> Ecto.Query.where(user_id: ^user_id, friend_id: ^friend_id, status: "request")
+      |> Repo.one!()
+      |> Ecto.Changeset.change(status: "friend") # |> IO.inspect()
+      |> Repo.update!()
+
+      %User{id: friend_id}
+      |> Ecto.build_assoc(:request_users, friend_id: user_id, status: "friend")
+      |> Repo.insert!()
+    end)
+  end
+
+  # Отклоняем запрос в друзья и удалаем запрос из базы дружбы
+  def reject_frend_request(user_id, friend_id) when user_id != friend_id do
+    with %User{id: ^user_id} <- Repo.get(User, user_id),
+    %User{id: ^friend_id} <- Repo.get(User, friend_id) do
+      User.Friendship
+      |> Ecto.Query.where(user_id: ^user_id, friend_id: ^friend_id) #|> IO.inspect()
+      |> Repo.one() # нужно сначала достать запись, а потом делать delete, без этого не работает
+      |> Repo.delete()
+    else
+      nil -> {:error, "User id #{user_id} or friend id #{friend_id} not found"}
+    end
+  end
+
+  # Проверяем кто наши друзья
+  def who_my_friends(user_id) do
+    with %User{id: ^user_id} <- Repo.get(User, user_id) do
+      User
+      |> Repo.get(user_id)
+      |> Repo.preload(:my_friends)
+    else
+      nil -> {:error, "User id #{user_id} not found"}
+    end
+  end
+
+  # Удаляем из друзей друг друга за одну транзакцию
+  def delete_from_friends(user_id, friend_id) when user_id != friend_id do
+    with %User{id: ^user_id} <- Repo.get(User, user_id),
+    %User{id: ^friend_id} <- Repo.get(User, friend_id) do
+      Repo.transaction(fn ->
+        Ecto.Query.from(u in User.Friendship, where: [user_id: ^user_id, friend_id: ^friend_id, status: "friend"],
+        or_where: [user_id: ^friend_id, friend_id: ^user_id, status: "friend"])
+        |> Repo.delete_all()
+      end)
+    else
+      nil -> {:error, "User id #{user_id} or friend id #{friend_id} not found"}
+    end
   end
 end
