1115b5b9f5006b9c0a9a7439fed6fcaa39acc88d
Sergey Afonkin <afonkinsergey@gmail.com>
2019-09-02 00:49:32 +0300

    clean-up comment from Session file

--- lib/jotter/user/session.ex
+++ lib/jotter/user/session.ex
@@ -7,19 +7,5 @@ defmodule Jotter.User.Session do
     else
       _ -> {:error, "Incorrect login credentials"}
     end
-
-    # user <- Repo.get_by(User, login: String.downcase(args.login))
-
-    # case check_password(user, args) do
-    #   true -> {:ok, user}
-    #   _    -> {:error, "Incorrect login credentials"}
-    # end
   end
-
-  # defp check_password(user, args) do
-  #   case user do
-  #     nil -> Comeonin.Argon2.dummy_checkpw()
-  #     _   -> Comeonin.Argon2.checkpw(args.password, user.password_hash)
-  #   end
-  # end
 end
