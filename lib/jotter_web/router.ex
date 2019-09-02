04bf8988faee52004ebd4b91fb94090c005a9852
Sergey Afonkin <afonkinsergey@gmail.com>
2019-09-02 23:50:06 +0300

    edit router for api access and add plug

--- lib/jotter_web/router.ex
+++ lib/jotter_web/router.ex
@@ -1,17 +1,28 @@
 defmodule JotterWeb.Router do
   use JotterWeb, :router
 
-  scope "/" do
-  forward "/graphiql",
-    Absinthe.Plug.GraphiQL,
-    schema: JotterWeb.Schema
+  # scope "/" do
+  # forward "/graphiql",
+  #   Absinthe.Plug.GraphiQL,
+  #   schema: JotterWeb.Schema
+  # end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+    plug JotterWeb.Plugs.Context
   end
 
-  # pipeline :api do
-  #   plug :accepts, ["json"]
-  # end
+  scope "/api" do
+    pipe_through :api
 
-  # scope "/api", JotterWeb do
-  #   pipe_through :api
-  # end
+    forward "/graphql",
+      Absinthe.Plug,
+      schema: JotterWeb.Schema
+
+      if Mix.env() == :dev do
+        forward "/graphiql",
+        Absinthe.Plug.GraphiQL,
+        schema: JotterWeb.Schema
+      end
+  end
 end
