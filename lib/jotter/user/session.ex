64955bd72be5fa805a7ba9b35cbd2ab428d1b6bf
Sergey Afonkin <afonkinsergey@gmail.com>
2019-09-02 23:56:31 +0300

    corrected auth metod

--- lib/jotter/user/session.ex
+++ lib/jotter/user/session.ex
@@ -1,11 +1,13 @@
 defmodule Jotter.User.Session do
   alias Jotter.{Repo, User}
 
-  def authenticate(args) do
-    with %User{} = user <- Repo.get_by(User, login: args.login, password: args.password) do
+  def authenticate(%{login: login, password: password}) do
+    with %User{} = user <- Repo.get_by(User, login: login, password: password) do
       {:ok, user}
     else
       _ -> {:error, "Incorrect login credentials"}
     end
   end
+
+  def authenticate(_), do: {:error, "Incorrect login credentials"}
 end
