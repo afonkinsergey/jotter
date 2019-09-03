fb5aa3b8d59c954ae741bb7a1333bd3bf6659e2c
Sergey Afonkin <afonkinsergey@gmail.com>
2019-09-03 13:41:18 +0300

    implement authorization Middleware

---
+++ lib/jotter_web/schema/middleware/authorize.ex
@@ -0,0 +1,11 @@
+defmodule JotterWeb.Schema.Middleware.Authorize do
+  @behaviour Absinthe.Middleware
+
+  def call(resolution, _) do
+    with %{current_user: _current_user} <- resolution.context do
+      resolution
+    else
+      _ -> resolution |> Absinthe.Resolution.put_result({:error, "unauthorized"})
+    end
+  end
+end
