bcd296d2db7836d1472cbad454c1f50d88ccbc1a
Sergey Afonkin <afonkinsergey@gmail.com>
2019-09-03 11:55:36 +0300

    вернул изменения роутера + добавил пайп плаг

--- lib/jotter_web/router.ex
+++ lib/jotter_web/router.ex
@@ -1,28 +1,30 @@
 defmodule JotterWeb.Router do
   use JotterWeb, :router
 
-  # scope "/" do
-  # forward "/graphiql",
-  #   Absinthe.Plug.GraphiQL,
-  #   schema: JotterWeb.Schema
-  # end
+  scope "/" do
+    pipe_through :api
+
+  forward "/graphiql",
+    Absinthe.Plug.GraphiQL,
+    schema: JotterWeb.Schema
+  end
 
   pipeline :api do
     plug :accepts, ["json"]
     plug JotterWeb.Plugs.Context
   end
 
-  scope "/api" do
-    pipe_through :api
+  # scope "/api" do
+  #   pipe_through :api
 
-    forward "/graphql",
-      Absinthe.Plug,
-      schema: JotterWeb.Schema
+  #   forward "/graphql",
+  #     Absinthe.Plug,
+  #     schema: JotterWeb.Schema
 
-      if Mix.env() == :dev do
-        forward "/graphiql",
-        Absinthe.Plug.GraphiQL,
-        schema: JotterWeb.Schema
-      end
-  end
+  #     if Mix.env() == :dev do
+  #       forward "/graphiql",
+  #       Absinthe.Plug.GraphiQL,
+  #       schema: JotterWeb.Schema
+  #     end
+  # end
 end
